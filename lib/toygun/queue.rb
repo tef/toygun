9bbf0e5e8abd217ce2e729b748ab9a4b682aa995
tef <tef@heroku.com>
2016-07-06 22:26:25 -0700

    queue

--- lib/toygun/queue.rb
+++ lib/toygun/queue.rb
@@ -1,2 +1,28 @@
 module Toygun
+  class RedisQueue
+    def initialize(redis:, queue:)
+      @redis = redis
+      @queue = queue
+    end
+
+    attr_reader :redis, :queue
+
+    def push(msg)
+      redis.rpush queue, msg
+    end
+
+    def size
+      redis.llen queue
+    end
+
+    def clear!
+      redis.del queue
+    end
+
+    def pop
+      msg = redis.blpop
+    end
+  end
+  
+
 end
